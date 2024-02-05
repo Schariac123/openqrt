47688b498b5387070e77d779b36a4d25f39e88fc
Schariac123 <142001984+Schariac123@users.noreply.github.com>
2024-02-05 12:22:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+#echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy' >>feeds.conf.default
 
 
